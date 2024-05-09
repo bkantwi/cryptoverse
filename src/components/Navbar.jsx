cea1325685f577e37a0d681735d6fbce88b21885
bkantwi <bernardantwi2010@gmail.com>
2024-05-09 22:06:03 +0000

    update on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
-import { link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/crypto.png';
